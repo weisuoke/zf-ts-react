f8a7bd6d4cab3482895332c76db190a8b18f27a7
梅林的法杖 <530264127@qq.com>
2020-12-27 20:52:05 +0800

    feat: 🎸 add redux-thunk

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,8 +1,33 @@
-import {applyMiddleware, createStore} from 'redux';
-import combinedReducer from '@/store/reducers';
+import {
+  createStore,
+  applyMiddleware,
+  Action,
+  Store,
+  AnyAction,
+  StoreEnhancer,
+  StoreEnhancerStoreCreator
+} from 'redux';
+import combinedReducer, { CombinedState } from '@/store/reducers';
 import { routerMiddleware } from 'connected-react-router';
 import history from '@/history';
+import thunk, { ThunkAction, ThunkDispatch } from 'redux-thunk';
 
-const store = applyMiddleware(routerMiddleware(history))(createStore)(combinedReducer);
+interface Ext {
+  dispatch: ThunkDispatch<CombinedState, undefined, AnyAction>
+}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface StateExt { }
+
+const storeEnhancer: StoreEnhancer = applyMiddleware(routerMiddleware(history), thunk);
+const storeEnhancerStoreCreator: StoreEnhancerStoreCreator = storeEnhancer(createStore);
+const store: Store<CombinedState & StateExt, AnyAction> & Ext = storeEnhancerStoreCreator(combinedReducer);
+const thunkAction: ThunkAction<void, CombinedState, undefined, AnyAction> = (
+  dispatch: ThunkDispatch<CombinedState, undefined, AnyAction>,
+  getState: () => CombinedState
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+): void => { };
+
+store.dispatch(thunkAction);
 
 export default store;
